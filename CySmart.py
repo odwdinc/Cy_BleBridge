aca299d63b1d34b80f7c6de1c58197f773cedbf7
odwdinc <anthony.pray@gmail.com>
2015-08-07 17:13:45 -0700

    added extracting name and address form scan data
    now allow for extracting of scaned data.

--- CySmart.py
+++ CySmart.py
@@ -3,11 +3,18 @@ import time
 import binascii
 class CySmart:
     Commands = {
-        'CMD_INIT_BLE_STACK':binascii.unhexlify("435907FC0000"),
-        'CMD_START_SCAN':binascii.unhexlify("435993FE0000"),
-        'CMD_STOP_SCAN':binascii.unhexlify("435994FE0000")
+        'CMD_Hedder':binascii.unhexlify("4359"),
+        'CMD_Footer':binascii.unhexlify("0000"),
+        'CMD_INIT_BLE_STACK':binascii.unhexlify("07FC"),
+        'CMD_START_SCAN':binascii.unhexlify("93FE"),
+        'CMD_STOP_SCAN':binascii.unhexlify("94FE")
     }
-
+    
+    
+    CYSMT_EVT_HEADER_CODE = binascii.unhexlify("BDA7")
+    EVT_SCAN_PROGRESS_RESULT = binascii.unhexlify("8A06")
+    EVT_COMMAND_STATUS =  binascii.unhexlify("7E04")
+    
 
     def __init__(self, ComPort='\\.\COM6'):
         self.serin = serial.Serial(ComPort, 115200, timeout=1)
@@ -20,7 +27,7 @@ class CySmart:
         return self.hexPrint(s).split(":")
     
     def sendCommand(self,command,timeout = 3):
-        self.serin.write(command)
+        self.serin.write(self.Commands['CMD_Hedder']+command+self.Commands['CMD_Footer'])
         self.serin.flush()
         out = ''
         check_timout = 0
@@ -34,16 +41,29 @@ class CySmart:
             return ">>:( " + out +" ) 0x>>:"+ self.hexPrint(out), out
         else:
             return "",out
-        
+    def getScanData(self,inputString):
+        Ble = {'address':[], 'name':""}
+        if self.CYSMT_EVT_HEADER_CODE in inputString:
+            prossesData = inputString.split(self.CYSMT_EVT_HEADER_CODE)
+            for pd in prossesData[1:]:
+                les = self.hexArray(pd)
+                if int(les[0],16) > 0x0E:
+                    if self.Commands['CMD_START_SCAN'] in pd:
+                        data_set = pd.split(self.Commands['CMD_START_SCAN'])[1]
+                        Ble['address'] =  self.hexArray(data_set)[1:6]
+                        if '\t' in data_set:
+                            nm_length=  int(self.hexArray(data_set.split('\t')[0])[-1],16)-1
+                            Ble['name'] = data_set.split('\t')[1][0:nm_length]
+        return Ble
+    
     def close(self):
         self.serin.close()
         
         
 cy = CySmart()
 cy.sendCommand(cy.Commands['CMD_INIT_BLE_STACK'])
-stro, ops = cy.sendCommand(cy.Commands['CMD_START_SCAN'],10)
-
-print (ops, cy.hexArray(ops))
-
+stro, ops = cy.sendCommand(cy.Commands['CMD_START_SCAN'])
 cy.sendCommand(cy.Commands['CMD_STOP_SCAN'])
 cy.close()
+
+print cy.getScanData(ops)
